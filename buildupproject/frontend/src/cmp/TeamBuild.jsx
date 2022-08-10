ae0b5c859985956d6788e2f9c5496435c114cf9d
jeongwoo903 <www8565@naver.com>
2022-08-10 19:30:26 +0900

    aa
    aa

--- buildupproject/frontend/src/cmp/TeamBuild.jsx
+++ buildupproject/frontend/src/cmp/TeamBuild.jsx
@@ -1,7 +1,63 @@
 import React from "react";
+<<<<<<< Updated upstream
 
 const TeamBuild = () => {
   return <>{/*  여기 작업해 주세요! */}</>;
+=======
+import {
+  Header,
+  HeaderTop,
+  HeaderRightDiv,
+  HeaderRightIcon,
+  HeaderTextBox,
+  Footer,
+  Asd,
+} from "../styledComponents";
+
+import { faUserGroup, faBookmark, faEllipsis, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const TeamBuild = () => {
+  return (
+    <>
+      <Header>
+        <HeaderTop>
+          <img className="headerlog" src="/img/logo.png" alt="buildup_logo.png" />
+          <HeaderRightDiv>
+            <HeaderRightIcon>
+              <FontAwesomeIcon icon={faMagnifyingGlass} />
+            </HeaderRightIcon>
+            <HeaderRightIcon>
+              <FontAwesomeIcon icon={faEllipsis} />
+            </HeaderRightIcon>
+          </HeaderRightDiv>
+        </HeaderTop>
+
+        <HeaderTextBox>
+          <li className="score600 lifirst">공모전</li>
+          <li className="score600">대외활동</li>
+          <li className="score600">사이드프로젝트</li>
+          <li className="score600">창업</li>
+        </HeaderTextBox>
+      </Header>
+      <Asd></Asd>
+      <Footer>
+        <button>
+          <img src="/img/bottomicon.png" alt="bottomicon.png" style={{ height: "40px" }} />
+        </button>
+        <button>
+          <FontAwesomeIcon className="fa-2x" icon={faBookmark} />
+        </button>
+        <button>
+          <FontAwesomeIcon className="fa-2x" icon={faUserGroup} />
+        </button>
+        <button className="userimgcover">
+          <img src="/img/user.png" alt="user.png" className="userimg" />
+        </button>
+      </Footer>
+    </>
+  );
+>>>>>>> Stashed changes
 };
 
 export default TeamBuild;
--- buildupproject/frontend/src/styledComponents.js
+++ buildupproject/frontend/src/styledComponents.js
@@ -53,3 +53,91 @@ export const MainButton = styled.button`
     } */
 `
 
+<<<<<<< Updated upstream
+=======
+// 상단바
+export const Header = styled.div`
+    margin-top: 20px!important;
+    width: 100%;
+    height: auto !important;
+`
+
+export const HeaderTop = styled.div`
+    display: flex;
+    position: relative;
+    
+    .headerlog {
+        max-width: 100%;
+        width:13%;
+        height: auto;
+        margin: 0 auto;
+    }
+`
+
+export const HeaderRightDiv = styled.div`
+    position : absolute;
+    margin-top: 6px;
+    right: 7%;
+    display: flex;
+`
+
+export const HeaderRightIcon = styled.button`
+    border: none;
+    color: #A6A6A6;
+    font-size: 140%;
+    background-color: transparent;
+    margin-left:16px;
+`
+
+export const HeaderTextBox = styled.ul`
+    display:flex;
+    justify-content: space-between;
+    list-style-type: none;
+    padding: 0 23px 0 23px;
+    margin-top: 20px;
+    color: #868687;
+    font-size: 95%;
+    border-bottom: 1px solid #D0CECE;
+
+    .lifirst {
+        color: black;
+        border-bottom: 3px solid #EE793F;
+        padding-bottom: 9px;
+    }
+`
+
+export const Asd = styled.div`
+    // flex: 1;
+`
+
+
+export const Footer = styled.div`
+    list-style-type: none;
+    height: 61px !important;
+    display : flex;
+    justify-content: space-around;
+    width: 100%;
+    border-top: 1px solid #d0cece;
+
+    button {
+        background-color: transparent;
+        outline: none;
+        border: none;
+    }
+
+    .userimgcover {
+        display: flex;
+        align-items: center;
+        width: fit-content;
+        height: 100%; 
+        overflow: hidden;
+    }
+
+    .userimg {
+        width: inherit;
+        height: 40px;
+        border: 1px solid #d0cece;
+        border-radius: 100px;
+    }
+`
+>>>>>>> Stashed changes
